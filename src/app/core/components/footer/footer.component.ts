51e97e6879382cffb4ff39b859846790ff51d980
ArunVash <arun.vashishtha@nagarro.com>
2024-03-11 17:52:54 +0530

    change for demo

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-  testGUID = '68d08d1e-8d7c-4ef8-9063-6fcd1a1a8deb';
+  testGUID = 'TestGUid';
 }
